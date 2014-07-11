8e6009c711a10f850fd37ca7142b9f6d2ed90cbc
Brent Cook <busterb@gmail.com>
2014-07-10 20:58:58 -0500

    axe unneeded solaris compatibility definitions

--- include/sys/types.h
+++ include/sys/types.h
@@ -5,11 +5,6 @@
 
 #include <stdint.h>
 
-#ifdef __sun
-typedef uint8_t u_int8_t;
-typedef uint32_t u_int32_t;
-#endif
-
 #if !defined(HAVE_ATTRIBUTE__BOUNDED__) && !defined(__bounded__)
 # define __bounded__(x, y, z)
 #endif

9c2c499bf3141fbcb61f4ca3fdbc77d451768bc8
Brent Cook <bcook@openbsd.org>
2014-07-20 13:40:14 -0500

    Use correct static link order for unit tests.
    thanks to Jonas 'Sortie' Termansen
    
    ok beck@

--- update.sh
+++ update.sh
@@ -307,8 +307,8 @@ test_excludes=(biotest aeadtest evptest pq_test ssltest arc4randomforktest fork_
 		fi
 		echo "check_PROGRAMS += $TEST" >> Makefile.am
 		echo "${TEST}_SOURCES = $i" >> Makefile.am
-		echo "${TEST}_LDADD = \$(top_builddir)/ssl/libssl.la" >> Makefile.am
-		echo "${TEST}_LDADD += \$(top_builddir)/crypto/libcrypto.la" >> Makefile.am
+		echo "${TEST}_LDADD = \$(top_builddir)/crypto/libcrypto.la" >> Makefile.am
+		echo "${TEST}_LDADD += \$(top_builddir)/ssl/libssl.la" >> Makefile.am
 	done
 )
 cp $libcrypto_regress/evp/evptests.txt tests

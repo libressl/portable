5390bbea3edcbafd48c5cb54f1e73070af78da04
Dmitry Eremin-Solenikov <dbaryshkov@gmail.com>
2014-12-08 02:07:21 +0300

    update.sh: call $CP instead of cp to ease rebuilds
    In several additional places call $CP instead of just cp to ease and
    speed up rebuilds after update.sh execution.
    
    Signed-off-by: Dmitry Eremin-Solenikov <dbaryshkov@gmail.com>

--- update.sh
+++ update.sh
@@ -101,7 +101,7 @@ for i in `awk '/SOURCES|HEADERS/ { print $3 }' crypto/Makefile.am` ; do
 	mkdir -p crypto/$dir
 	if [ $dir != "compat" ]; then
 		if [ -e $libssl_src/src/crypto/$i ]; then
-			cp $libssl_src/src/crypto/$i crypto/$i
+			$CP $libssl_src/src/crypto/$i crypto/$i
 		fi
 	fi
 done
@@ -137,7 +137,7 @@ done
 echo copying libtls source
 rm -f tls/*.c tls/*.h
 for i in `awk '/SOURCES|HEADERS/ { print $3 }' tls/Makefile.am` ; do
-	cp $libtls_src/$i tls
+	$CP $libtls_src/$i tls
 done
 
 # copy openssl(1) source
@@ -146,7 +146,7 @@ $CP $libc_src/stdlib/strtonum.c apps
 $CP $libcrypto_src/openssl.cnf apps
 for i in `awk '/SOURCES|HEADERS/ { print $3 }' apps/Makefile.am` ; do
 	if [ -e $openssl_app_src/$i ]; then
-		cp $openssl_app_src/$i apps
+		$CP $openssl_app_src/$i apps
 	fi
 done
 
@@ -154,7 +154,7 @@ done
 echo "copying libssl source"
 rm -f ssl/*.c ssl/*.h
 for i in `awk '/SOURCES|HEADERS/ { print $3 }' ssl/Makefile.am` ; do
-	cp $libssl_src/src/ssl/$i ssl
+	$CP $libssl_src/src/ssl/$i ssl
 done
 
 # copy libcrypto tests
@@ -263,7 +263,7 @@ echo "copying manpages"
 	# update new-style manpages
 	for i in `ls -1 $libssl_src/src/doc/ssl/*.3 | sort`; do
 		NAME=`basename "$i"`
-		cp $i .
+		$CP $i .
 		echo "dist_man_MANS += $NAME" >> Makefile.am
 	done
 	$CP $openssl_app_src/openssl.1 .

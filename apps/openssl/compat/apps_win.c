a896d400a0bd65512e7c779306aae7480ee079b4
Brent Cook <bcook@openbsd.org>
2015-09-13 09:19:26 -0500

    move windows file IO mode setup to apps_win.c

--- apps/openssl/compat/apps_win.c
+++ apps/openssl/compat/apps_win.c
@@ -7,6 +7,9 @@
 
 #include <windows.h>
 
+#include <io.h>
+#include <fcntl.h>
+
 #include "apps.h"
 
 double
@@ -26,4 +29,32 @@ app_tminterval(int stop, int usertime)
 		return (ut.u64 + kt.u64 - tmstart) / (double) 10000000;
 	}
 	return 0;
+}
+
+int
+setup_ui(void)
+{
+	ui_method = UI_create_method("OpenSSL application user interface");
+	UI_method_set_opener(ui_method, ui_open);
+	UI_method_set_reader(ui_method, ui_read);
+	UI_method_set_writer(ui_method, ui_write);
+	UI_method_set_closer(ui_method, ui_close);
+
+	/*
+	 * Set STDIO to binary
+	 */
+	_setmode(_fileno(stdin), _O_BINARY);
+	_setmode(_fileno(stdout), _O_BINARY);
+	_setmode(_fileno(stderr), _O_BINARY);
+
+	return 0;
+}
+
+void
+destroy_ui(void)
+{
+	if (ui_method) {
+		UI_destroy_method(ui_method);
+		ui_method = NULL;
+	}
 }

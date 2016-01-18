8c91563f603f7021110da231f80e503713340aa6
Brent Cook <bcook@openbsd.org>
2016-01-18 09:50:23 -0600

    add solaris include for getentropy(2)

--- include/compat/unistd.h
+++ include/compat/unistd.h
@@ -27,6 +27,13 @@ unsigned int sleep(unsigned int seconds);
 
 #ifndef HAVE_GETENTROPY
 int getentropy(void *buf, size_t buflen);
+#else
+/*
+ * Solaris 11.3 adds getentropy(2), but defines the function in sys/random.h
+ */
+#if defined(__sun)
+#include <sys/random.h>
+#endif
 #endif
 
 #define pledge(request, paths) 0

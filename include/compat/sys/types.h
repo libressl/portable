6f24864a7301efa60a5722e08172f9554653ede6
Brent Cook <busterb@gmail.com>
2016-09-04 07:57:13 -0500

    cdefs.h should not be required anywhere

--- include/compat/sys/types.h
+++ include/compat/sys/types.h
@@ -46,4 +46,20 @@ typedef SSIZE_T ssize_t;
 # define __bounded__(x, y, z)
 #endif
 
+#ifdef _WIN32
+#define __warn_references(sym,msg)
+#else
+
+#ifndef __warn_references
+#if defined(__GNUC__)  && defined (HAS_GNU_WARNING_LONG)
+#define __warn_references(sym,msg)          \
+  __asm__(".section .gnu.warning." __STRING(sym)  \
+         " ; .ascii \"" msg "\" ; .text");
+#else
+#define __warn_references(sym,msg)
+#endif
+
+#endif /* __warn_references */
+#endif /* _WIN32 */
+
 #endif

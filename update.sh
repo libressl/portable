5f187770f36e5049f54a3c717e8c71e3c824e5ba
kinichiro <kinichiro.inoguchi@gmail.com>
2016-11-06 04:57:23 +0900

    Add curve25519 to portable

--- update.sh
+++ update.sh
@@ -124,7 +124,7 @@ copy_hdrs $libcrypto_src "stack/stack.h lhash/lhash.h stack/safestack.h
 	md4/md4.h ripemd/ripemd.h whrlpool/whrlpool.h idea/idea.h
 	rc2/rc2.h rc4/rc4.h ui/ui_compat.h txt_db/txt_db.h
 	chacha/chacha.h evp/evp.h poly1305/poly1305.h camellia/camellia.h
-	gost/gost.h"
+	gost/gost.h curve25519/curve25519.h"
 
 copy_hdrs $libssl_src "srtp.h ssl.h ssl2.h ssl3.h ssl23.h tls1.h dtls1.h"
 

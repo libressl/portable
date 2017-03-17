764ab1e6a9978f68eac491214bb5619ffc24774d
Brent Cook <bcook@openbsd.org>
2017-03-16 19:25:42 -0500

    remove sysconf fallback for now

--- crypto/compat/getpagesize.c
+++ crypto/compat/getpagesize.c
@@ -1,18 +1,12 @@
 /* $OpenBSD$ */
 
 #include <unistd.h>
-
-#ifdef _MSC_VER
 #include <windows.h>
-#endif
 
 int
-getpagesize(void) {
-#ifdef _MSC_VER
+getpagesize(void)
+{
 	SYSTEM_INFO system_info;
 	GetSystemInfo(&system_info);
 	return system_info.dwPageSize;
-#else
-	return sysconf(_SC_PAGESIZE);
-#endif
 }

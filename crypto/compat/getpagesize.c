da995141dc0992cf86f2e029fcb9b05077709fb0
Brent Cook <bcook@openbsd.org>
2017-06-11 11:21:34 -0500

    re-add getpagesize fallback, needed for Android

--- crypto/compat/getpagesize.c
+++ crypto/compat/getpagesize.c
@@ -1,12 +1,18 @@
 /* $OpenBSD$ */
 
 #include <unistd.h>
+
+#ifdef _MSC_VER
 #include <windows.h>
+#endif
 
 int
-getpagesize(void)
-{
+getpagesize(void) {
+#ifdef _MSC_VER
 	SYSTEM_INFO system_info;
 	GetSystemInfo(&system_info);
 	return system_info.dwPageSize;
+#else
+	return sysconf(_SC_PAGESIZE);
+#endif
 }

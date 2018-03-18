0e46c1e8ba9eca009173d2dc0c3084b322088eaf
Brent Cook <busterb@gmail.com>
2018-03-18 10:36:51 -0500

    add pthread_self/pthread_equal for win32

--- include/compat/pthread.h
+++ include/compat/pthread.h
@@ -18,23 +18,43 @@
 struct pthread_once {
 	INIT_ONCE once;
 };
-
 typedef struct pthread_once pthread_once_t;
 
-static BOOL CALLBACK _pthread_once_win32_cb(PINIT_ONCE once, PVOID param, PVOID *context)
+static inline BOOL CALLBACK
+_pthread_once_win32_cb(PINIT_ONCE once, PVOID param, PVOID *context)
 {
 	void (*cb) (void) = param;
 	cb();
 	return TRUE;
 }
 
-static int pthread_once(pthread_once_t *once, void (*cb) (void))
+static inline int
+pthread_once(pthread_once_t *once, void (*cb) (void))
 {
 	BOOL rc = InitOnceExecuteOnce(&once->once, _pthread_once_win32_cb, cb, NULL);
 	if (rc == 0)
 		return -1;
 	else
 		return 0;
+}
+
+struct pthread {
+	HANDLE handle;
+};
+typedef struct pthread pthread_t;
+
+static inline pthread_t
+pthread_self(void)
+{
+	pthread_t self;
+	self.handle = GetCurrentThread();
+	return self;
+}
+
+static inline int
+pthread_equal(pthread_t t1, pthread_t t2)
+{
+	return t1.handle == t2.handle;
 }
 
 #else

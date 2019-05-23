c199315ee9c3dbac0397cb426d905f5b2b66436f
Brent Cook <busterb@gmail.com>
2019-05-23 06:58:38 -0500

    condense android comments for clarity

--- crypto/compat/getprogname_linux.c
+++ crypto/compat/getprogname_linux.c
@@ -5,23 +5,16 @@
 const char *
 getprogname(void)
 {
+#if defined(__ANDROID_API__) && __ANDROID_API__ < 21
 	/*
-	 * Android added getprogname with API 21 [0]. We should not end up here
-	 * with APIs bigger than 21. Still write a precise check.
+	 * Android added getprogname with API 21, so we should not end up here
+	 * with APIs newer than 21.
+	 * https://github.com/aosp-mirror/platform_bionic/blob/1eb6d3/libc/include/stdlib.h#L160
 	 *
 	 * Since Android is using portions of OpenBSD libc, it should have
-	 * a symbol called __progname [1].
-	 *
-	 *
-	 * .. [0] https://github.com/aosp-mirror/platform_bionic/blob/1eb6d3/libc/include/stdlib.h#L160
-	 *
-	 * .. [1] https://github.com/aosp-mirror/platform_bionic/commit/692207
-	 *
-	 * .. [2] https://linux.die.net/man/3/program_invocation_short_name
-	 *
-	 * .. [3] https://android.googlesource.com/platform/system/core/+/2819c0/base/logging.cpp#65
+	 * a symbol called __progname.
+	 * https://github.com/aosp-mirror/platform_bionic/commit/692207
 	 */
-#if defined(__ANDROID_API__) && __ANDROID_API__ < 21
 	extern const char *__progname;
 	return __progname;
 #else

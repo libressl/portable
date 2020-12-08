c9c6d2fc4dca77b3c7e363d6d003e93e3271fef2
Brent Cook <busterb@gmail.com>
2020-12-08 11:05:55 -0600

    LibreSSL 3.1.5

--- patches/opensslv.h
+++ patches/opensslv.h
@@ -3,9 +3,9 @@
 #define HEADER_OPENSSLV_H
 
 /* These will change with each release of LibreSSL-portable */
-#define LIBRESSL_VERSION_NUMBER 0x3010400fL
+#define LIBRESSL_VERSION_NUMBER 0x3010500fL
 /*                                    ^ Patch starts here   */
-#define LIBRESSL_VERSION_TEXT   "LibreSSL 3.1.4"
+#define LIBRESSL_VERSION_TEXT   "LibreSSL 3.1.5"
 
 /* These will never change */
 #define OPENSSL_VERSION_NUMBER	0x20000000L

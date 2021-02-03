99e74b5b2ef13732dc86987c44c4151b66f25c3a
Brent Cook <busterb@gmail.com>
2021-02-03 06:22:12 -0600

    update version to LibreSSL 3.2.4

--- patches/opensslv.h
+++ patches/opensslv.h
@@ -3,9 +3,9 @@
 #define HEADER_OPENSSLV_H
 
 /* These will change with each release of LibreSSL-portable */
-#define LIBRESSL_VERSION_NUMBER 0x3020300fL
+#define LIBRESSL_VERSION_NUMBER 0x3020400fL
 /*                                    ^ Patch starts here   */
-#define LIBRESSL_VERSION_TEXT   "LibreSSL 3.2.3"
+#define LIBRESSL_VERSION_TEXT   "LibreSSL 3.2.4"
 
 /* These will never change */
 #define OPENSSL_VERSION_NUMBER	0x20000000L

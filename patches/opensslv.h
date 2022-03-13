394639f4eb4d7d324700ecb2f7900f7516139f8f
Brent Cook <busterb@gmail.com>
2022-03-13 12:35:20 -0500

    add changelog

--- patches/opensslv.h
+++ patches/opensslv.h
@@ -3,9 +3,9 @@
 #define HEADER_OPENSSLV_H
 
 /* These will change with each release of LibreSSL-portable */
-#define LIBRESSL_VERSION_NUMBER 0x3040200fL
+#define LIBRESSL_VERSION_NUMBER 0x3040300fL
 /*                                    ^ Patch starts here   */
-#define LIBRESSL_VERSION_TEXT   "LibreSSL 3.4.2"
+#define LIBRESSL_VERSION_TEXT   "LibreSSL 3.4.3"
 
 /* These will never change */
 #define OPENSSL_VERSION_NUMBER	0x20000000L

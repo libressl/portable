5f5feb2bbc94a8cd8d101b6245c0d9b50f2a15ce
Brent Cook <busterb@gmail.com>
2022-07-10 22:13:46 -0500

    update Windows certs

--- tests/ssltest.bat
+++ tests/ssltest.bat
@@ -10,8 +10,8 @@ set openssl_bin=%2
 set openssl_bin=%openssl_bin:/=\%
 if not exist %openssl_bin% exit /b 1
 
-%srcdir%\testssl.bat %srcdir%\server.pem %srcdir%\server.pem %srcdir%\ca.pem ^
-    %ssltest_bin% %openssl_bin%
+%srcdir%\testssl.bat %srcdir%\server1-rsa.pem %srcdir%\server1-rsa-chain.pem ^
+	%srcdir%\ca-root-rsa.pem %ssltest_bin% %openssl_bin%
 if !errorlevel! neq 0 (
 	exit /b 1
 )

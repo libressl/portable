aa1f9b11664ac41f304b638ec259d937fee6281e
Brent Cook <busterb@gmail.com>
2022-10-24 19:38:56 -0500

    LibreSSL 3.6.1

---
+++ patches/opensslv.h
@@ -0,0 +1,18 @@
+/* $OpenBSD: opensslv.h,v 1.70 2022/07/04 12:31:55 tb Exp $ */
+#ifndef HEADER_OPENSSLV_H
+#define HEADER_OPENSSLV_H
+
+/* These will change with each release of LibreSSL-portable */
+#define LIBRESSL_VERSION_NUMBER 0x3060100fL
+/*                                    ^ Patch starts here   */
+#define LIBRESSL_VERSION_TEXT   "LibreSSL 3.6.1"
+
+/* These will never change */
+#define OPENSSL_VERSION_NUMBER	0x20000000L
+#define OPENSSL_VERSION_TEXT	LIBRESSL_VERSION_TEXT
+#define OPENSSL_VERSION_PTEXT	" part of " OPENSSL_VERSION_TEXT
+
+#define SHLIB_VERSION_HISTORY ""
+#define SHLIB_VERSION_NUMBER "1.0.0"
+
+#endif /* HEADER_OPENSSLV_H */

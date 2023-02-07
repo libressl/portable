42dff66511fc63e7461f3043c49f1c90f3b5115b
Brent Cook <busterb@gmail.com>
2023-02-06 21:30:59 -0600

    bump version

--- patches/opensslv.h
+++ patches/opensslv.h
@@ -3,9 +3,9 @@
 #define HEADER_OPENSSLV_H
 
 /* These will change with each release of LibreSSL-portable */
-#define LIBRESSL_VERSION_NUMBER 0x3060100fL
+#define LIBRESSL_VERSION_NUMBER 0x3060200fL
 /*                                    ^ Patch starts here   */
-#define LIBRESSL_VERSION_TEXT   "LibreSSL 3.6.1"
+#define LIBRESSL_VERSION_TEXT   "LibreSSL 3.6.2"
 
 /* These will never change */
 #define OPENSSL_VERSION_NUMBER	0x20000000L

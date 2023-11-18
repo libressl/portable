0a446e81ed77c20aa87563d45ef0ef8f5fa283d8
Theo Buehler <tb@openbsd.org>
2023-11-18 10:53:09 +0100

    Prefix some compat symbols with libressl_
    See #928. This isn't a full fix, but should remove much of the friction
    already.

--- include/compat/stdlib.h
+++ include/compat/stdlib.h
@@ -20,26 +20,36 @@
 #include <stdint.h>
 
 #ifndef HAVE_ARC4RANDOM_BUF
+#define arc4random libressl_arc4random
 uint32_t arc4random(void);
+#define arc4random_buf libressl_arc4random_buf
 void arc4random_buf(void *_buf, size_t n);
+#define arc4random_uniform libressl_arc4random_uniform
 uint32_t arc4random_uniform(uint32_t upper_bound);
 #endif
 
 #ifndef HAVE_FREEZERO
+#define freezero libressl_freezero
 void freezero(void *ptr, size_t sz);
 #endif
 
 #ifndef HAVE_GETPROGNAME
+#define getprogname libressl_getprogname
 const char * getprogname(void);
 #endif
 
+#ifndef HAVE_REALLOCARRAY
+#define reallocarray libressl_reallocarray
 void *reallocarray(void *, size_t, size_t);
+#endif
 
 #ifndef HAVE_RECALLOCARRAY
+#define recallocarray libressl_recallocarray
 void *recallocarray(void *, size_t, size_t, size_t);
 #endif
 
 #ifndef HAVE_STRTONUM
+#define strtonum libressl_strtonum
 long long strtonum(const char *nptr, long long minval,
 		long long maxval, const char **errstr);
 #endif

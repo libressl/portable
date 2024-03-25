0e30a6f66d0d9d95e586dcc9d01d2de8afc68cd6
Brent Cook <busterb@gmail.com>
2024-03-24 21:26:56 -0500

    bump version for LibreSSL 3.9.1

---
+++ patches/opensslv.h
@@ -0,0 +1,18 @@
+/* $OpenBSD: opensslv.h,v 1.78 2024/03/02 11:53:30 tb Exp $ */
+#ifndef HEADER_OPENSSLV_H
+#define HEADER_OPENSSLV_H
+
+/* These will change with each release of LibreSSL-portable */
+#define LIBRESSL_VERSION_NUMBER 0x3090100fL
+/*                                    ^ Patch starts here   */
+#define LIBRESSL_VERSION_TEXT   "LibreSSL 3.9.1"
+
+/* These will never change */
+#define OPENSSL_VERSION_NUMBER	0x20000000L
+#define OPENSSL_VERSION_TEXT	LIBRESSL_VERSION_TEXT
+#define OPENSSL_VERSION_PTEXT	" part of " OPENSSL_VERSION_TEXT
+
+#define SHLIB_VERSION_HISTORY ""
+#define SHLIB_VERSION_NUMBER "1.0.0"
+
+#endif /* HEADER_OPENSSLV_H */

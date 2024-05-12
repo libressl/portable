22a03075732f7397f2bacbc5353116c04a1a4156
Brent Cook <busterb@gmail.com>
2024-05-11 22:13:30 -0500

    LibreSSL 3.9.2

--- patches/opensslv.h
+++ patches/opensslv.h
@@ -3,9 +3,9 @@
 #define HEADER_OPENSSLV_H
 
 /* These will change with each release of LibreSSL-portable */
-#define LIBRESSL_VERSION_NUMBER 0x3090100fL
+#define LIBRESSL_VERSION_NUMBER 0x3090200fL
 /*                                    ^ Patch starts here   */
-#define LIBRESSL_VERSION_TEXT   "LibreSSL 3.9.1"
+#define LIBRESSL_VERSION_TEXT   "LibreSSL 3.9.2"
 
 /* These will never change */
 #define OPENSSL_VERSION_NUMBER	0x20000000L

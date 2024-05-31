eab5ace757bba8f5491e1a6c1381875c4aeebb07
MoustaphaSaad <moustapha.saad.abdelhamed@gmail.com>
2024-05-31 19:52:59 +0300

    fix ssltest
    changed the ssltest.sh to search to .js file as well.
    added preload-file link flag to enable access to TEST_SOURCE_DIR

--- tests/ssltest.sh
+++ tests/ssltest.sh
@@ -4,17 +4,23 @@ set -e
 ssltest_bin=./ssltest
 if [ -e ./ssltest.exe ]; then
 	ssltest_bin=./ssltest.exe
+elif [ -e ./ssltest.js ]; then
+	ssltest_bin="node ./ssltest.js"
 fi
 
 if [ -d ../apps/openssl ]; then
 	openssl_bin=../apps/openssl/openssl
 	if [ -e ../apps/openssl/openssl.exe ]; then
 		openssl_bin=../apps/openssl/openssl.exe
+	elif [ -e ../apps/openssl/openssl.js ]; then
+		openssl_bin="node ../apps/openssl/openssl.js"
 	fi
 else
 	openssl_bin=../apps/openssl
 	if [ -e ../apps/openssl.exe ]; then
 		openssl_bin=../apps/openssl.exe
+	elif [ -e ../apps/openssl.js ]; then
+		openssl_bin="node ../apps/openssl.js"
 	fi
 fi
 
@@ -24,4 +30,4 @@ fi
 
 $srcdir/testssl $srcdir/server1-rsa.pem $srcdir/server1-rsa-chain.pem \
 	$srcdir/ca-root-rsa.pem \
-	$ssltest_bin $openssl_bin
+	"$ssltest_bin" "$openssl_bin"

8de367893bf8198d0959d4fabed9847b90209ae0
Theo Buehler <tb@openbsd.org>
2024-11-16 08:20:29 -0700

    Disable sha512 perlasm

--- update.sh
+++ update.sh
@@ -284,7 +284,6 @@ for abi in elf macosx masm mingw64; do
 	gen_asm_stdout $abi modes/asm/ghash-x86_64.pl    modes/ghash-$abi-x86_64.S
 	gen_asm_stdout $abi rc4/asm/rc4-x86_64.pl        rc4/rc4-$abi-x86_64.S
 	gen_asm_stdout $abi sha/asm/sha1-x86_64.pl       sha/sha1-$abi-x86_64.S
-	gen_asm        $abi sha/asm/sha512-x86_64.pl     sha/sha512-$abi-x86_64.S
 done
 
 # copy libtls source

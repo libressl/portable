320776e8a47d8005758b06046e3ccd06c5600897
Sunny-Maxis <masuji@pm.me>
2025-10-22 12:53:33 -0400

    SHA-512 update for getentropy_irix.c
    Rewrote latter section according to feedback.

--- crypto/compat/getentropy_irix.c
+++ crypto/compat/getentropy_irix.c
@@ -1,4 +1,6 @@
 /*
+ * Copyright (c) 2014 Theo de Raadt <deraadt@openbsd.org>
+ * Copyright (c) 2014 Bob Beck <beck@obtuse.com>
  * Copyright (c) 2025 Kazuo Kuroi (kazuo@irixnet.org)
  *
  * Permission to use, copy, modify, and distribute this software for any
@@ -15,23 +17,33 @@
  *
  *
  * getentropy_irix.c --  getentropy function for IRIX 6.5 and up. 
+ * some parts based off getentropy_linux.c
  * 
  */ 
 
-
-#include <sys/types.h>                                                                                       
-#include <sys/stat.h>                                                                                        
-#include <sys/time.h>          
-#include <sys/utsname.h>                                                                                     
-#include <sys/sysmp.h>                     
-#include <sys/syssgi.h>
+#include <sys/types.h>
+#include <sys/time.h>
+#include <sys/utsname.h>
+#include <sys/sysmp.h>
 #include <unistd.h>
-#include <fcntl.h>     
+#include <fcntl.h>
 #include <errno.h>
 #include <string.h>
-#include <stdlib.h>              
+#include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+
+/* Use LibreSSL's SHA-512 implementation */
+typedef struct {
+    unsigned long long h[8];
+    unsigned long long Nl, Nh;
+    unsigned char data[128];
+    unsigned int num;
+} SHA512_CTX;
+
+int  SHA512_Init(SHA512_CTX *c);
+int  SHA512_Update(SHA512_CTX *c, const void *data, size_t len);
+int  SHA512_Final(unsigned char *md, SHA512_CTX *c);
 
 int
 getentropy(void *buf, size_t len)
@@ -40,53 +52,79 @@ getentropy(void *buf, size_t len)
     ssize_t ret;
     size_t i = 0;
     int save_errno = errno;
+    unsigned char *out = buf;
 
-    /* Primary: use /dev/urandom */
+    if (len > 256) { /* OpenBSD length compliance */
+        errno = EINVAL;
+        return (-1);
+    }
+
+    /* Try /dev/urandom first */
     fd = open("/dev/urandom", O_RDONLY);
     if (fd != -1) {
         while (i < len) {
-            ret = read(fd, (char *)buf + i, len - i);
-            if (ret <= 0) {
-                if (errno == EINTR) continue;
+            ret = read(fd, out + i, len - i);
+            if (ret < 0) {
+                if (errno == EINTR)
+                    continue;
                 break;
             }
-            i += ret;
+            if (ret == 0)
+                break;
+            i += (size_t)ret;
         }
         close(fd);
         if (i == len) {
             errno = save_errno;
-            return 0;
+            return (0);
         }
     }
 
-    /* Fallback: hacky entropy in case urandom/random isn't available */
+    /* Fallback code */
     struct timeval tv;
     struct utsname un;
     pid_t pid = getpid(), ppid = getppid();
     uid_t uid = getuid(), gid = getgid();
     long nprocs = sysmp(MP_NPROCS);
-    unsigned long tsc = 0;
+    time_t now = time(NULL);
+    void *stackptr = &fd;
 
-    uname(&un);
     gettimeofday(&tv, NULL);
+    uname(&un);
 
-    unsigned char *p = buf;
-    size_t minlen = len < sizeof(tv) + sizeof(pid) + sizeof(tsc) + sizeof(uid) ? len : sizeof(tv) + sizeof(pid) + sizeof(tsc) + sizeof(uid);
-    unsigned char mix[256];
-    memset(mix, 0, sizeof(mix));
+    unsigned char pool[512];
+    memset(pool, 0, sizeof(pool));
+    size_t pos = 0;
 
-    /* Combine multiple system values */
-    snprintf((char *)mix, sizeof(mix), "%ld%ld%lu%s%d%d%d%ld%s",
-        (long)tv.tv_sec, (long)tv.tv_usec, tsc,
-        un.nodename, pid, ppid, uid, nprocs, un.version);
+    pos += snprintf((char *)pool + pos, sizeof(pool) - pos,
+        "%ld:%ld:%d:%d:%d:%d:%ld:%ld:%s:%s:%p",
+        (long)tv.tv_sec, (long)tv.tv_usec,
+        (int)pid, (int)ppid, (int)uid, (int)gid,
+        nprocs, (long)now,
+        un.nodename, un.version, stackptr);
 
-    /* Simple linear congruential mix for extra diffusion */
-    unsigned int seed = (unsigned int)tsc ^ pid ^ (tv.tv_usec + (tv.tv_sec << 16));
-    for (i = 0; i < len; i++) {
-        seed = 1103515245 * seed + 12345 + mix[i % sizeof(mix)];
-        p[i] = (unsigned char)(seed >> 16);
+    /* Hash the pool */
+    unsigned char digest[64];
+    unsigned char counter = 0;
+    SHA512_CTX ctx;
+
+    size_t remaining = len;
+    while (remaining > 0) {
+        SHA512_Init(&ctx);
+        SHA512_Update(&ctx, pool, pos);
+        SHA512_Update(&ctx, &counter, sizeof(counter));
+        SHA512_Final(digest, &ctx);
+        counter++;
+
+        size_t take = remaining < sizeof(digest) ? remaining : sizeof(digest);
+        memcpy(out + (len - remaining), digest, take);
+        remaining -= take;
     }
+    /* cleanup */
+    memset(pool, 0, sizeof(pool));
+    memset(digest, 0, sizeof(digest));
+    memset(&ctx, 0, sizeof(ctx));
 
     errno = save_errno;
-    return 0;
+    return (0);
 }

43f336dd00b31182cbe54da31f89298dca9c43c4
Kartik Naik <kartik@bugqore.com>
2026-07-15 18:38:33 +0530

    restore file offset on error in pread/pwrite shims

--- tls/compat/pwrite.c
+++ tls/compat/pwrite.c
@@ -8,21 +8,25 @@
 
 #define NO_REDEF_POSIX_FUNCTIONS
 
+#include <errno.h>
 #include <unistd.h>
 
 ssize_t
 pwrite(int d, const void *buf, size_t nbytes, off_t offset)
 {
-	off_t cpos, opos, rpos;
+	off_t cpos;
 	ssize_t bytes;
+	int save_errno;
+
 	if((cpos = lseek(d, 0, SEEK_CUR)) == -1)
 		return -1;
-	if((opos = lseek(d, offset, SEEK_SET)) == -1)
+	if(lseek(d, offset, SEEK_SET) == -1)
 		return -1;
-	if((bytes = write(d, buf, nbytes)) == -1)
+	bytes = write(d, buf, nbytes);
+	save_errno = errno;
+	if(lseek(d, cpos, SEEK_SET) == -1)
 		return -1;
-	if((rpos = lseek(d, cpos, SEEK_SET)) == -1)
-		return -1;
+	errno = save_errno;
 	return bytes;
 }
 

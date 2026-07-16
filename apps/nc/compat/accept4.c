5c7dd432a6d2141a422b40c5e33ff2e43c1b9667
Kartik Naik <kartik@bugqore.com>
2026-07-16 19:33:10 +0530

    honor SOCK_NONBLOCK in the accept4 shim

--- apps/nc/compat/accept4.c
+++ apps/nc/compat/accept4.c
@@ -5,12 +5,18 @@ int
 accept4(int s, struct sockaddr *addr, socklen_t *addrlen, int flags)
 {
 	int rets = accept(s, addr, addrlen);
+	int fl;
 	if (rets == -1)
 		return rets;
 
 	if (flags & SOCK_CLOEXEC) {
-		flags = fcntl(rets, F_GETFD);
-		fcntl(rets, F_SETFD, flags | FD_CLOEXEC);
+		fl = fcntl(rets, F_GETFD);
+		fcntl(rets, F_SETFD, fl | FD_CLOEXEC);
+	}
+
+	if (flags & SOCK_NONBLOCK) {
+		fl = fcntl(rets, F_GETFL);
+		fcntl(rets, F_SETFL, fl | O_NONBLOCK);
 	}
 
 	return rets;

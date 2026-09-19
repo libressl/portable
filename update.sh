8240fe007fd3ae26f2ce106a5742dd4b37cbb5d8
Brent Cook <busterb@gmail.com>
2026-09-19 18:28:25 -0500

    add a hook for inserting a release candidate version
    This adds a hook to the release script so we can create an 'rc' package
    by specifying LIBRESSL_RC like so:
    
     % LIBRESSL_RC=1 ./dist.sh
    pulling upstream openbsd source
    Not on a tag, grabbing latest (NOTE: this may be broken from time to time)
    Already on 'master'
    Your branch is up to date with 'origin/master'.
    Already up to date.
    libcrypto version 57:2:0
    libssl version 60:2:0
    libtls version 33:2:0
    LibreSSL version 4.3.1-rc1
    ....

--- update.sh
+++ update.sh
@@ -164,6 +164,9 @@ else
 fi
 
 awk '/LIBRESSL_VERSION_TEXT/ {print $4}' < include/openssl/opensslv.h | cut -d\" -f1 | head -n1 > VERSION
+if [ -n "$LIBRESSL_RC" ]; then
+	printf '%s-rc%s\n' "`cat VERSION`" "$LIBRESSL_RC" > VERSION
+fi
 echo "LibreSSL version `cat VERSION`"
 
 # copy libcrypto source

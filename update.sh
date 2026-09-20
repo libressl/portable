1ec9900751e1d7a931db48ddbe1d334b23132aeb
Brent Cook <busterb@gmail.com>
2026-09-20 14:39:44 -0500

    update header with RC version in TEXT constant

--- update.sh
+++ update.sh
@@ -166,6 +166,8 @@ fi
 awk '/LIBRESSL_VERSION_TEXT/ {print $4}' < include/openssl/opensslv.h | cut -d\" -f1 | head -n1 > VERSION
 if [ -n "$LIBRESSL_RC" ]; then
 	printf '%src%s\n' "`cat VERSION`" "$LIBRESSL_RC" > VERSION
+	sed -E "s/(#define LIBRESSL_VERSION_TEXT[[:space:]]+\"LibreSSL )[^\"]*\"/\1`cat VERSION`\"/" include/openssl/opensslv.h > include/openssl/opensslv.h.tmp
+	mv include/openssl/opensslv.h.tmp include/openssl/opensslv.h
 fi
 echo "LibreSSL version `cat VERSION`"
 

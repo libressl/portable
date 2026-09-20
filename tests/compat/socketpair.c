830a761c97cbfd65d03c2134c2858fdef6b81700
Brent Cook <busterb@gmail.com>
2026-09-20 13:02:38 -0500

    split pipe2 from socketpair detection
    Support macOS 27's new pipe2 that can build but not work if you have SDK
    27 installed on 26. This verifies it actually works before linking it.
    Split socketpair from pipe2, since they can now be independently
    available.

---
+++ tests/compat/socketpair.c
@@ -0,0 +1,193 @@
+/*
+ * Public domain
+ *
+ * socketpair emulation
+ * Brent Cook <bcook@openbsd.org>
+ */
+
+#include <errno.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/socket.h>
+
+#undef socketpair
+
+#ifdef _WIN32
+
+static int setfd(int fd, int flag)
+{
+	int rc = -1;
+	if (flag & FD_CLOEXEC) {
+		/* fd is a Winsock SOCKET, not a CRT descriptor: use it as a
+		 * handle directly rather than translating with _get_osfhandle. */
+		HANDLE h = (HANDLE)(LONG_PTR)fd;
+		rc = SetHandleInformation(h, HANDLE_FLAG_INHERIT, 0) == 0 ? -1 : 0;
+	}
+	return rc;
+}
+
+static int setfl(int fd, int flag)
+{
+	int rc = -1;
+	if (flag & O_NONBLOCK) {
+		long mode = 1;
+		rc = ioctlsocket(fd, FIONBIO, &mode);
+	}
+	return rc;
+}
+
+/*
+ * Have open() temporarily use up file descriptors until reaching beyond the
+ * allocated sockets, then leak the ones conflicting with any of the latter.
+ *
+ * open()/close() are redefined to posix_open()/posix_close() in this file,
+ * which tag descriptors with the 0x80000000 bit. Use the raw CRT _open/_close
+ * so the values compare against the socket handles in the same namespace.
+ */
+static void create_issue_1069_sentinels(int socket_vector[2])
+{
+	int fd = _open("CONIN$", O_RDONLY);
+	if (fd == -1 || (fd > socket_vector[0] && fd > socket_vector[1])) {
+		return;
+	}
+	create_issue_1069_sentinels(socket_vector);
+	if (fd != socket_vector[0] && fd != socket_vector[1]) {
+		_close(fd);
+	}
+}
+
+int socketpair(int domain, int type, int protocol, int socket_vector[2])
+{
+	if (domain != AF_UNIX || !(type & SOCK_STREAM) || protocol != PF_UNSPEC)
+		return -1;
+
+	socket_vector[0] = -1;
+	socket_vector[1] = -1;
+
+	int listener = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (listener == -1) {
+		return -1;
+	}
+
+	struct sockaddr_in addr = {
+		.sin_family = AF_INET,
+		.sin_addr.s_addr = htonl(INADDR_LOOPBACK),
+		.sin_port = 0,
+	};
+
+	struct sockaddr_in self, peer;
+	int yes = 1, e;
+
+	/*
+	 * SO_REUSEADDR does not mean here what it means on unix: it lets any
+	 * other process bind the same address and port and take over the
+	 * rendezvous. SO_EXCLUSIVEADDRUSE is the flag that keeps the port ours.
+	 */
+	if (setsockopt(listener, SOL_SOCKET, SO_EXCLUSIVEADDRUSE,
+			(void *)&yes, sizeof yes) == -1)
+		goto err;
+
+	if (bind(listener, (struct sockaddr *)&addr, sizeof addr) != 0)
+		goto err;
+
+	memset(&addr, 0, sizeof addr);
+	socklen_t addrlen = sizeof addr;
+	if (getsockname(listener, (struct sockaddr *)&addr, &addrlen) != 0)
+		goto err;
+
+	addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+	addr.sin_family = AF_INET;
+
+	if (listen(listener, 1) != 0)
+		goto err;
+
+	socket_vector[0] = WSASocket(AF_INET, SOCK_STREAM, 0, NULL, 0, 0);
+	if (socket_vector[0] == -1)
+		goto err;
+
+	if (connect(socket_vector[0], (struct sockaddr *)&addr, sizeof addr) != 0)
+		goto err;
+
+	socket_vector[1] = accept(listener, NULL, NULL);
+	if (socket_vector[1] == -1)
+		goto err;
+
+	/*
+	 * The listening port is enumerable by anything running as the user, so
+	 * the connection we just accepted is not necessarily the one we made.
+	 * Pair the two halves only if they are each other's peer.
+	 */
+	memset(&self, 0, sizeof self);
+	addrlen = sizeof self;
+	if (getsockname(socket_vector[0], (struct sockaddr *)&self, &addrlen) != 0)
+		goto err;
+
+	memset(&peer, 0, sizeof peer);
+	addrlen = sizeof peer;
+	if (getpeername(socket_vector[1], (struct sockaddr *)&peer, &addrlen) != 0)
+		goto err;
+
+	if (self.sin_family != peer.sin_family ||
+	    self.sin_addr.s_addr != peer.sin_addr.s_addr ||
+	    self.sin_port != peer.sin_port) {
+		WSASetLastError(WSAECONNREFUSED);
+		goto err;
+	}
+
+	closesocket(listener);
+
+	create_issue_1069_sentinels(socket_vector);
+
+	return 0;
+
+err:
+	e = WSAGetLastError();
+	closesocket(listener);
+	closesocket(socket_vector[0]);
+	closesocket(socket_vector[1]);
+	WSASetLastError(e);
+	socket_vector[0] = -1;
+	socket_vector[1] = -1;
+	return -1;
+}
+
+#else
+
+static int setfd(int fd, int flag)
+{
+	int flags = fcntl(fd, F_GETFD);
+	flags |= flag;
+	return fcntl(fd, F_SETFD, flags);
+}
+
+static int setfl(int fd, int flag)
+{
+	int flags = fcntl(fd, F_GETFL);
+	flags |= flag;
+	return fcntl(fd, F_SETFL, flags);
+}
+#endif
+
+int bsd_socketpair(int domain, int type, int protocol, int socket_vector[2])
+{
+	int flags = type & ~0xf;
+	type &= 0xf;
+	int rc = socketpair(domain, type, protocol, socket_vector);
+	if (rc == 0) {
+		if (flags & SOCK_NONBLOCK) {
+			rc |= setfl(socket_vector[0], O_NONBLOCK);
+			rc |= setfl(socket_vector[1], O_NONBLOCK);
+		}
+		if (flags & SOCK_CLOEXEC) {
+			rc |= setfd(socket_vector[0], FD_CLOEXEC);
+			rc |= setfd(socket_vector[1], FD_CLOEXEC);
+		}
+		if (rc != 0) {
+			int e = errno;
+			close(socket_vector[0]);
+			close(socket_vector[1]);
+			errno = e;
+		}
+	}
+	return rc;
+}
